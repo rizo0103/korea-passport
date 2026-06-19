86a02df6d0f149b4cc8e75c514756a18855acd12
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2026-06-19 12:07:13 +0500

    added navigator and home screen

---
+++ android/src/constants/colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+  primary: "#004A94",
+  accent: "#901D2F",
+
+  background: "#0B0F1A",
+  surface: "#121A2A",
+
+  textPrimary: "#FFFFFF",
+  textSecondary: "#AAB3C5",
+};
